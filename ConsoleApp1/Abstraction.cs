fc78a639237692d69ca39310936431b710f8449c
asev07 <77210324+asev07@users.noreply.github.com>
2022-07-05 16:01:41 +0300

    abstraction assignment

--- ConsoleApp1/Abstraction.cs
+++ ConsoleApp1/Abstraction.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ABSTRACTION
+
 {
-    internal class Abstraction
+    abstract class IShape
     {
+        public abstract float area();
+        public abstract float perimeter();
+    }
+    public class rectangle : IShape
+    {
+        public float width { set; get };
+        public float length { set; get };
+
+
+        public override float area()
+        {
+            return length * width;
+        }
+
+        public override float perimeter()
+        {
+            return 2 * (length + width);
+        }
+    }
+
+    public class Circle : IShape
+    {
+        public static readonly float pi = 3.14;
+
+        public float radius { set; get };
+
+
+        public override float area()
+        {
+            return pi * (radius * radius);
+        }
+
+        public override float perimeter()
+        {
+            return 2 * (pi * radius);
+        }
     }
 }
+
+
+
+
+
+
+
